dade0a86ddb9d586fef768a2f0f994056b3e2e37
tanim913 <tanim.khaled24@gmail.com>
2022-08-29 16:07:07 +0600

    leetcode 15

---
+++ leetcode/leetcode_14_reverse-bits.py
@@ -0,0 +1,12 @@
+class Solution:
+    def reverseBits(self, n: int) -> int:
+        res = 0
+        
+        
+        for i in range (32):
+            res <<= 1
+            if n&1 == 1:
+                res += 1
+            n >>= 1
+    
+        return res
