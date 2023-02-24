448a17ed72eaa7a10fb60c503fec3097442a9e49
K.M. Azizullah Tanim <tanim.aziz913@gmail.com>
2023-02-24 17:03:24 +0600

    contains duplicate description

--- leetcode/leetcode_3_contains-duplicate.py
+++ leetcode/leetcode_3_contains-duplicate.py
@@ -1,3 +1,9 @@
+'''
+    set in python only stores unique values as it's element. 
+    so when we are about to add an element to the set, we need to check if
+    that specific element already exist in the set. If it does then the list contain
+    duplicates 
+'''
 class Solution:
     List = []
 
