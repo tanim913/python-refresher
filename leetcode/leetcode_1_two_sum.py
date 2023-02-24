704bc7eb3e202febbae070b38c96904fdbf1385b
K.M. Azizullah Tanim <tanim.aziz913@gmail.com>
2023-02-24 16:21:44 +0600

    two sum description

--- leetcode/leetcode_1_two_sum.py
+++ leetcode/leetcode_1_two_sum.py
@@ -1,4 +1,11 @@
+''' as we need to return a list of the index of the given list which contains numbers, 
+    we need to create a map which would be contiously adding the index of the number
+    until it gets the index of the difference from the target value to the currnt number
+    of the list
 
+    T.C : O(n) ; iterating through all the numbers of the string
+    S.C : O(n) ; using a hashmap to potentially add every value to the hashmap from the list
+'''
 class Solution:
     List = []
 
